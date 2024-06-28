78ec3e5e6a8c4162acefc6284ce0462093d1b30d
NISHIZAWA Shuntaro <me@s2n.tech>
2024-06-28 14:56:15 +0900

    feat: Initial commit

---
+++ scripts/register.ts
@@ -0,0 +1,37 @@
+import {
+  ApplicationCommandOptionType,
+  RESTPostAPIApplicationCommandsJSONBody,
+  Routes,
+} from "discord-api-types/v10";
+import { REST } from "@discordjs/rest";
+import { config } from "dotenv";
+
+config();
+
+const superchat: RESTPostAPIApplicationCommandsJSONBody = {
+  name: "superchat",
+  description: "スーパーチャットを送ります",
+  options: [
+    {
+      type: ApplicationCommandOptionType.Integer,
+      name: "金額",
+      description: "スーパーチャットの金額",
+      required: true,
+    },
+    {
+      type: ApplicationCommandOptionType.String,
+      name: "コメント",
+      description: "スーパーチャットのコメント",
+    },
+  ],
+};
+
+const token = process.env["DISCORD_TOKEN"]!;
+const applicationId = process.env["DISCORD_APPLICATION_ID"]!;
+const guildId = process.env["DISCORD_GUILD_ID"]!;
+
+const rest = new REST({ version: "10" }).setToken(token);
+
+await rest.put(Routes.applicationGuildCommands(applicationId, guildId), {
+  body: [superchat],
+});
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { Hono } from "hono";
+import { verifyKey } from "discord-interactions";
+import type { APIInteraction } from "discord-api-types/v10";
+import {
+  InteractionType,
+  InteractionResponseType,
+  APIInteractionResponse,
+} from "discord-api-types/v10";
+
+type Bindings = {
+  DISCORD_PUBLIC_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>()
+  .get("/", (c) => {
+    return c.text("OK");
+  })
+  .post(
+    "/interaction",
+    async (c, next) => {
+      const signature = c.req.header("X-Signature-Ed25519")!;
+      const timestamp = c.req.header("X-Signature-Timestamp")!;
+      const body = await c.req.text();
+      const isValid = await verifyKey(
+        body,
+        signature,
+        timestamp,
+        c.env["DISCORD_PUBLIC_KEY"]
+      );
+
+      if (!isValid) {
+        return c.json({ status: 401, message: "Unauthorized" }, 401);
+      }
+
+      return next();
+    },
+    async (c) => {
+      const interaction: APIInteraction = await c.req.json();
+
+      if (interaction.type === InteractionType.Ping) {
+        return c.json<APIInteractionResponse>({
+          type: InteractionResponseType.Pong,
+        });
+      }
+
+      if (
+        interaction.type === InteractionType.ApplicationCommand &&
+        interaction.data.name === "superchat"
+      ) {
+        return c.json<APIInteractionResponse>({
+          type: InteractionResponseType.ChannelMessageWithSource,
+          data: {
+            content: "スーパーチャットを送りました！",
+          },
+        });
+      }
+
+      return c.json({ status: 400, message: "Bad Request" }, 400);
+    }
+  );
+
+export default app;
